bf69370f1dbbecff4e775c2b0397d4af491ee8ba
Patrick <patricknwosu360@gmail.com>
2024-01-04 03:32:33 +0100

    program that prints numbers from 0 to 99.

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for x in range(0, 100):
+for x in range(0, 99):
     formatted = str(x).zfill(2)
-    print("{}".format(formatted))
+    print("{}, ".format(formatted), end='')
+print("99")
