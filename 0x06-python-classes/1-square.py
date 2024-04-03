cb9f76efe3258f2d8492d1838532f101a90c7807
Patrick <patricknwosu360@gmail.com>
2024-04-03 15:52:31 +0100

    new

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#1-square by patrick
+"""Defines a Square based on 0-square"""
+
+class Square:
+    """Defines a square"""
+    def __init__(self,size):
+        """Initialize the square class
+        Args:Size- represents the size of a square
+        """
+        self.__size = size
