443416f40dd19530f11a6bc98a113ba37e519386
Patrick <patricknwosu360@gmail.com>
2024-04-19 22:58:48 +0100

    new

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,5 @@
-#usr/bin/python3
-
-"""This returns a list of attribute and methods"""
+#!/usr/bin/python3
+"""A modulue that returns a list of attributes and methods"""
 def lookup(obj):
-    """This returns a list of attributes"""
+    """Returns a list"""
     return dir(obj)
