7d8d7f5926d8eeff80f87df87d1cbbbe275769fc
Patrick <patricknwosu360@gmail.com>
2024-04-20 18:07:55 +0100

    new

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""A module that contains a function that writes a string to a text file (UTF8) 
+and returns the number of characters written"""
+
+def write_file(filename="", text=""):
+    """A function that writes to a file"""
+    if filename:
+        with open(filename, "w", encoding="utf-8") as file:
+            file.write(text\n)
+            return len(text)
