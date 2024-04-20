77b2a7689ce3d918054685237667d2980f45870b
Patrick <patricknwosu360@gmail.com>
2024-04-20 17:42:53 +0100

    new

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A module that reads a file"""
+
+def read_file(filename=""):
+    """This function reads a text
+    file and prints the result to 
+    stdout"""
+    if filename:
+        with open(filename, encoding="utf-8") as file:
+            for line in file:
+                print(line, end="")
