3d498c8e492286a0924ae0d547828cf626ca593a
Patrick <patricknwosu360@gmail.com>
2024-04-21 20:23:05 +0100

    new

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -2,6 +2,7 @@
 """A module that returns an object 
 (Python data structure) represented by a JSON string:"""
 
+import json
 def from_json_string(my_str):
     """A function that returns an object 
     (Python data structure) represented by a JSON string:"""
