4229283ea6209270e389b3402755ef116c3de4a4
Patrick <patricknwosu360@gmail.com>
2024-04-25 00:16:40 +0100

    ew

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -104,3 +104,11 @@ class Rectangle(Base):
     def area(self):
         """Returns the area of the rectangle"""
         return self.__width * self.__height
+
+    def display(self):
+        """Prints to stdout the rectangle with #"""
+        for i in range(self.__height):
+            for j in range(self.__width):
+                print("#", end="")
+            print()
+            
