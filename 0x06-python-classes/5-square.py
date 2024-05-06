facccc47612c0eac85dcec5e69c40eee7f4dc23e
Patrick <patricknwosu360@gmail.com>
2024-05-06 22:09:47 +0100

    new

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -2,18 +2,32 @@
 """Defines a based on square"""
 class Square:
     """A class that defines a square"""
+
     def __init__(self, size=0):
-        """Initializes an object"""
+        """Initializes an object
+
+        Args:
+            size (int): This is the size of the square
+
+        Raises:
+            TypeError: size must be an intger
+            ValueError: size must be >= 0
+
+        """
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must >= 0")
         else:
             self.__size = size
+
     @property
     def size(self):
         """Retrieves a property"""
+
         return self.__size
+
     @size.setter
     def size(self, value):
         """Sets a property value"""
@@ -23,11 +37,23 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
+
     def area(self):
-        """Returns the current area"""
+        """The area of a square
+
+        Return:
+            returns the area of a square
+        """
+
         return self.__size**2
+
     def my_print(self):
-        """print a square with #"""
+        """print a square with
+
+        Return:
+            returns the square with the character #
+        """
+
         if self.__size == 0:
             print()
         else:
