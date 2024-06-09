4107431b53a5cb1e486b592de13af02d39f59c23
Patrick <patricknwosu360@gmail.com>
2024-06-09 10:36:27 +0100

    new

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Defines a square"""
+
+
 class Square:
     """A class that defines a square"""
     def __init__(self, size=0):
@@ -10,19 +12,23 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+
     @property
     def size(self):
         """Retrieves Private instance attribute"""
         return self.__size
+
     @size.setter
-    def size(self,value):
+    def size(self, value):
         """Sets private instance attribute"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
+
     def area(self):
+
         """Returns the area"""
         return self.__size**2
