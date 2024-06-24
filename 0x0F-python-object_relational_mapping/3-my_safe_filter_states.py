a3d1a815c75fdadb53571f80a63c306d037739ed
Patrick <patricknwosu360@gmail.com>
2024-06-25 00:42:27 +0100

    new

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""a script that takes in an argument and displays all
-values in the states table of hbtn_0e_0_usa
-where name matches the argument."""
-
+""" a script that takes in arguments and
+displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument
+"""
 if __name__ == "__main__":
     import MySQLdb
     from sys import argv
@@ -14,10 +14,12 @@ if __name__ == "__main__":
     query = """
             SELECT id, name
             FROM states
-            WHERE name LIKE BINARY = {}
+            WHERE name = %s
             ORDER BY id ASC
-            """.format(argv[4])
-    cursor.execute(query)
+            """
+    cursor.execute(query, (argv[4],))
     rows = cursor.fetchall()
     for row in rows:
         print(row)
+    cursor.close()
+    db.close()
