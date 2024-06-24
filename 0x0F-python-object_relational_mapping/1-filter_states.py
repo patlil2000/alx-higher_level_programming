9494469ddadde3c4774f0729cbe91920c5a52fb6
Patrick <patricknwosu360@gmail.com>
2024-06-24 21:13:16 +0100

    new

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,10 +11,12 @@ if __name__ == "__main__":
                          passwd=argv[2],
                          db=argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT id, name
+    cursor.execute("""
+                   SELECT id, name
                    FROM states
                    WHERE LEFT(name, 1)='N'
-                   ORDER BY id ASC")
+                   ORDER BY id ASC
+                   """)
     rows = cursor.fetchall()
     for row in rows:
         print(row)
