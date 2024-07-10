176671bf7b158261b468ea4f96abea0114d8860b
Patrick <patricknwosu360@gmail.com>
2024-07-10 02:02:27 +0100

    new

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -8,6 +8,6 @@ from sys import argv
 
 if __name__ == "__main__":
     url = argv[1]
-    email = {'email' : argv[2]}
+    email = {'email': argv[2]}
     r = requests.post(url, data=email)
     print(r.text)
