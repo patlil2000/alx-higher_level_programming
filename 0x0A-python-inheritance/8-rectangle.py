c7cc8cabab1564341488bd7c92ae7d31708dad14
Patrick <patricknwosu360@gmail.com>
2024-07-21 03:35:19 +0100

    new

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-"""A module that defines a class"""
+"""a class Rectangle that
+inherits from BaseGeometry
+"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 
 class Rectangle(BaseGeometry):
-    """A class that inherits from a class BaseGeometry"""
+    """a class rectangle
+    """
     def __init__(self, width, height):
-        """Initializes the class"""
-        if not isinstance(width, int):
-            raise TypeError("width must be an integer")
-        elif width <= 0:
-            raise ValueError("width must be a positive integer")
-        else:
-            self.__width = width
-
-        if not isinstance(height, int):
-            raise TypeError("height must be an integer")
-        elif height <= 0:
-            raise ValueError("height must be a positive integer")
-        else:
-            self.__height = height
+        """Instantiation of object"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
