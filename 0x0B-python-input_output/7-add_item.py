f2d87eb97928cf73659a24b1b93b0b03de6a2871
Patrick <patricknwosu360@gmail.com>
2024-08-10 17:15:38 +0100

    new

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,7 +8,7 @@ import os
 
 
 save = __import__('5-save_to_json_file').save_to_json_file
-load =  __import__('6-load_from_json_file').load_from_json_file
+load = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
