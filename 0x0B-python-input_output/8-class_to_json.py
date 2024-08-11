2edc418e2ad36de0bfb4113008d4985cd9a299b0
Patrick <patricknwosu360@gmail.com>
2024-08-11 23:22:12 +0100

    new

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """A module that returns the dictionary description"""
 
+
 def class_to_json(obj):
     """returns a dictionary description"""
     dict_list = {}
     for attr, value in obj.__dict__.items():
-        if not isinstance(value, (list,dict,str,int,bool)):
+        if not isinstance(value, (list, dict, str, int, bool)):
             raise TypeError("Value is not a valid data structure")
         else:
             dict_list[attr] = value
     return dict_list
-
-
