0d1e61c479caa4fd654f231c13b9e10c94103596
Patrick <patricknwosu360@gmail.com>
2024-08-11 23:31:31 +0100

    new

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """A module that defines a class Student"""
 
+
 class Student:
     """A class student"""
     def __init__(self, first_name, last_name, age):
@@ -8,10 +9,10 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
     def to_json(self):
         """Returns a dictionary representattion"""
         dict_list = {}
         for attr, value in self.__dict__.items():
             dict_list[attr] = value
         return dict_list
-
