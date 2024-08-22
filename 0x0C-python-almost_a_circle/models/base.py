a8a29bf06e545b8b71749f2649dab4c3871df282
Patrick <patricknwosu360@gmail.com>
2024-08-22 03:51:33 +0100

    new

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -37,3 +37,12 @@ class Base:
         json_string = cls.to_json_string(list_dict)
         with open(filename, 'w') as f:
             f.write(json_string)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns  the list of the JSON
+        string representation json_string
+        """
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
